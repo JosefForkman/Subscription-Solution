19bd47b16ba2aa4f5aef6130b366100c27765316
JosefForkman <strand.vatten@outlook.com>
2023-09-13 04:19:19 +0100

    beginning of the login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,65 +1,69 @@
+import styles from "./login.module.css";
+import InputText from '@/components/Form/InputText'
 import Link from 'next/link'
+import Image from "next/image";
 import Messages from './messages'
+import facebook from "../assets/facebook.svg"
+import google from "../assets/google.svg"
+import apple from "../assets/apple.svg"
+
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+export const dynamic = 'force-dynamic'
 
 export default function Login() {
+  const supabase = createServerComponentClient({ cookies })
+  async function signInWithGoogle() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent',
+        },
+      },
+    })
+  }
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <Link
-        href="/"
-        className="absolute left-8 top-16 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{' '}
-        Back
-      </Link>
-
+    <>
       <form
-        className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+        className={styles.loginForm}
         action="/auth/sign-in"
         method="post"
       >
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <button className="bg-green-700 rounded px-4 py-2 text-white mb-2">
-          Sign In
+        <InputText placeholder='användarnamn' />
+        <InputText placeholder='lösenord' />
+
+        <p className="h4 text-light-gray text-center">
+          Har du glömt ditt lösenord?
+          <br />
+          <Link href='/'>Återställ det här.</Link>
+        </p>
+
+        {/* Message component not sure if we should use it */}
+        {/* <Messages /> */}
+        <button className={styles.btn + " " + "bg-accent text-white text-center h1"}>Logga in</button>
+      </form >
+
+      <p className={"text-center" + " " + styles.divider}>eller</p>
+
+      <section className={styles.socialMediaSection}>
+        <button onClick={signInWithGoogle} className={styles.btn + " " + "bg-white"}>
+          <Image width={44} height={44} src={google} alt="" />
+          <span className="h3">Logga in med google här</span>
         </button>
-        <button
-          formAction="/auth/sign-up"
-          className="border border-gray-700 rounded px-4 py-2 text-black mb-2"
-        >
-          Sign Up
+        <button className={styles.btn + " " + "bg-black"}>
+          <Image width={44} height={44} src={apple} alt="" />
+          <span className="h3 text-white">Logga in med apple här</span>
         </button>
-        <Messages />
-      </form>
-    </div>
+        <button className={styles.btn + " " + "bg-facebook"}>
+          <Image width={44} height={44} src={facebook} alt="" />
+          <span className="h3 text-white">Logga in med facebook här</span>
+        </button>
+
+        <p className="h4 text-center text-gray">Har du inte ett konto? <Link href="/sign-up">Skapa ett här</Link></p>
+      </section>
+
+    </>
   )
 }
