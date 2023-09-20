3eeddbbc2c9a12a7f5730856693c2bb939fab517
Styrbjörn Nordberg <styris.n@gmail.com>
2023-09-20 17:15:02 +0200

    added working query

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
 
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic';
 
 export default async function Index() {
-
-  const supabase = createServerComponentClient({cookies});
-  const {data} = await supabase.from("dummyTable").select();
+  const supabase = createServerComponentClient({ cookies });
+  const { data } = await supabase.from('user_service').select(`
+  *, 
+  service(*, category(*), price_history(*))
+  `);
   return (
     <>
       <h1>Home page</h1>
+      <p>{typeof(data)}</p>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </>
-  )
+  );
 }
