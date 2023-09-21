cbee2441818dad9688c79e0e667c43f98c48eb6e
Josef Forkman <strand.vatten@outlook.com>
2023-09-21 11:07:29 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function Index() {
     data: { session },
   } = await supabase.auth.getSession();
   if (!session) {
-    redirect('/sign-in');
+    redirect('/login');
   }
   const { data } = await supabase.from('user_service').select(`
   *, 
