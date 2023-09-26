52e14ac115fb693351873d0974133ed7c12a11a7
JosefForkman <strand.vatten@outlook.com>
2023-09-26 08:26:20 +0100

    fix tabs

--- lib/getTabs.ts
+++ lib/getTabs.ts
@@ -2,12 +2,18 @@ import { tabsType } from "./Context/filter";
 import { PrenumerationType } from "./Prenumerationer";
 
 export const getTabs = (prenumerationer: PrenumerationType[]): tabsType[] => {
-    const tabs = prenumerationer.map(value => {
-        return {
-            name: value.type ?? "error",
-            active: false
-        }
-    })
+    const tabs = prenumerationer
+        /* Make proper format of array */
+        .map((value) => {
+            return {
+                name: value.type ?? "error",
+                active: false,
+            };
+        })
+        /* Get uniq tabs */
+        .filter((value, index, array) => array
+            .findIndex(indexValue => indexValue.name == value.name) === index)
 
-    return tabs
-}
+            
+    return tabs;
+};
