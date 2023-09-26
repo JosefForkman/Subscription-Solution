17010eb114093b13dd6898674812b024925d6dec
Styrbjörn Nordberg <styris.n@gmail.com>
2023-09-26 15:48:14 +0200

    added build requirments

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import './globals.css'
 
 import { Inter } from 'next/font/google'
 
+
+export const dynamic = 'force-dynamic'
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ['latin'] })
 
